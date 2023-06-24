60c54c2c4aecb53b51b294bc52cbdcf10eea941a
Hope Golestany <hopegolestany@gmail.com>
2023-06-24 23:59:14 +0330

    base implemention of gin

--- gin.go
+++ gin.go
@@ -1,6 +1,11 @@
 package httpzen
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ginApp struct {
 	ginDomainHandlers []Module
@@ -15,4 +20,45 @@ type ginApp struct {
 	ExternalDocs *ExternalDocs
 	openApiRoute string
 	jsonOpenApi  string
+}
+
+func NewGinZen() Api {
+	return ginApp{}
+}
+
+func (ginApp) Run(ip string, port uint, mode string) error {
+	panic("not implemented")
+}
+func (ginApp) AppendModule(mod Module) {
+	panic("not implemented")
+}
+func (ginApp) AppendMiddleware(string, Action) {
+	panic("not implemented")
+}
+func (ginApp) GetRoute(url, method string) *RequestDefinition {
+	panic("not implemented")
+}
+func (ginApp) SetCors(cors []string) {
+	panic("not implemented")
+}
+func (ginApp) TestHandle(*httptest.ResponseRecorder, *http.Request) error {
+	panic("not implemented")
+}
+func (ginApp) SetExternalDocs(ExternalDocs) {
+	panic("not implemented")
+}
+func (ginApp) SetInfo(Info) {
+	panic("not implemented")
+}
+func (ginApp) SetContact(Contact) {
+	panic("not implemented")
+}
+func (ginApp) SetServers([]Server) {
+	panic("not implemented")
+}
+func (ginApp) EnableOpenApi(route string) error {
+	panic("not implemented")
+}
+func (ginApp) SetErrors([]string) {
+	panic("not implemented")
 }
